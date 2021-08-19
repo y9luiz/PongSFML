8ee49bbbdbbc96c4ac00bd2d3db1bed20e8b809e
Luiz Felipe Maciel Correia <y9luiz@hotmail.com>
2021-08-19 08:32:42 -0300

    created button class

--- button.cpp
+++ button.cpp
@@ -1,7 +1,17 @@
 #include "button.h"
 
-Button::Button(const int x, const int y, const int width, const int height, const std::string & text)
+Button::Button(const int x, const int y, const int width, const int height, const sf::Texture& texture) :
+    sf::Sprite(texture, sf::Rect<int>(x, y, width, height))
 {   
+    texture_ = std::make_shared<sf::Texture>(texture);
+}
+
+
+Button::Button(const int x, const int y, const int width, const int height)
+{
     
+    texture_ = std::make_shared<sf::Texture>();
+    setTextureRect(sf::Rect<int>(x,y,width,height));
+    setTexture(*texture_);
 }
 
--- button.h
+++ button.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-class Button : public sf::RectangleShape
+class Button : public sf::Sprite
 {
     public:
-        Button(const int x, const int y, const int width, const int height, const std::string & text);   
+        Button(const int x, const int y, const int width, const int height, const sf::Texture& texture);
+        Button(const int x, const int y, const int width, const int height);
+
     protected:
-        sf::Color color_;
-        sf::Text text_;
+        std::shared_ptr<sf::Texture>  texture_;
+
 };
