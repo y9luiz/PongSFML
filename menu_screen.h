13edf9a01bcf89fd68662bf35ebc1c3d0b53f1b1
Luiz Felipe Maciel Correia <y9luiz@hotmail.com>
2021-08-19 08:33:49 -0300

    button added to MenuScreen

--- menu_screen.h
+++ menu_screen.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "screen.h"
+#include "button.h"
 #include <SFML/Graphics.hpp>
 
 class MenuScreen : public Screen 
@@ -8,8 +9,14 @@ class MenuScreen : public Screen
     public:
         MenuScreen(const int width, const int height, const std::string tittle);
 		void run() override;
+        inline void setButton(const int x, const int y, const int width, const int height, sf::Texture & texture)
+        {
+            play_button_ = std::make_shared<Button>(x, y, width, height, texture);
+        }
     protected:
-        
+        std::shared_ptr<Button> play_button_;
+        bool is_running_ = false; 
         void handleInput() override;
+        sf::Texture texture;
 
 };
