ad3400c6f4e90ac93d8163453a9eb7239037f359
Edivânia Pontes <edivaniapts@gmail.com>
2021-08-20 20:56:45 -0300

    defs obstacles

--- defs.h
+++ defs.h
@@ -7,12 +7,13 @@ const std::string ASSERTS_PATH = "Assets";
 
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
-const sf::Vector2f PADDLE_SIZE(30,150);
+
 const std::string FONT_SCORE_PATH = "Assets/CollegiateOutlineFLF.ttf";
 const sf::Color FONT_SCORE_COLOR = sf::Color(255, 255, 255);
 const int FONT_SCORE_SIZE = 30;
 
 // PLAYERS DEFINITIONS
+const sf::Vector2f PADDLE_SIZE(30,150);
 const sf::Vector2f PADDLE_SPEED(1, 10);
 
 // PLAYER 1 DEFINITIONS 
@@ -34,3 +35,14 @@ const sf::Vector2f BALL_SPEED(15, 3);
 const sf::Color BALL_COLOR(0, 255, 0);
 
 //const sf::Vector2f BALL_INIT_POSITION = sf::Vector2f(PLAYER_1_X_POS, PLAYER_1_Y_POS);
+
+// OBSTACLES DEFINITIONS
+const sf::Color OBSTACLE_COLOR = sf::Color(69, 129, 142);
+const sf::Vector2f OBSTACLE_SIZE(15, 200);
+const sf::Vector2f LEVEL2_OBST_POS(10, 10);
+const sf::Vector2f LEVEL3_OBST1_POS(10, 10);
+const sf::Vector2f LEVEL3_OBST1_POS(10, 10);
+const sf::Vector2f LEVEL3_OBST2_POS(10, 10);
+const sf::Vector2f LEVEL4_OBST1_POS(10, 10);
+const sf::Vector2f LEVEL4_OBST2_POS(10, 10);
+
